38483eb6981d5d6c335baaf56b6ea2ec314de634
PotentialParadox <dtracy@ufl.edu>
2015-08-26 23:34:33 -0400

    Now opens as a floating point set

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 __author__ = 'dustintracy'
 import numpy as np
+import matplotlib.pyplot as plt
 import pylab
 import scipy.stats as stats
 
@@ -15,7 +16,18 @@ for line in infile:
 # Truncate and convert to numpy array
 data_array = np.array(data)
 
-data = data_array[:,0:22:3]
-A = data[0:2,0:12]
-print(A)
+data = data_array[:-1,[3,15,21]]
+data = np.array(data, dtype=float)
+data = abs(data)
+data_n = data[:,[0,1]]
+data_n[:,1] = data[:,2] - data[:,1]
+
+distance = data_n[:,0]
+difference = data_n[:,1]
+
+plt.scatter(distance, difference)
+plt.xlabel("1")
+plt.ylabel("1")
+plt.show()
+
 
